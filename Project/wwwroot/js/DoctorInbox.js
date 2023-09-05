d86c8b265b3c4c7c44093e814a3adf8bea602e26
muhammadabdulrehmansaleem <bsef20m033@pucit.edu.pk>
2023-09-05 10:34:20 -0700

    11th iteration

--- Project/wwwroot/js/DoctorInbox.js
+++ Project/wwwroot/js/DoctorInbox.js
@@ -4,8 +4,11 @@ var DoctorId;
 var PatientId;
 var DoctorName = "";
 var PatientName = "";
-function sendMessage(DoctorId,PatientId) {
-    connection.invoke("LoadChatForDoctor", DoctorId, PatientId).catch(err => console.error("Error calling LoadChatFunctionDoctor:", err));
+var User_role = "Doctor";
+function sendMessage(DoctorId, message, PatientId ) {
+    var ms = message;
+    console.log(typeof (ms));
+    connection.invoke("SendMessage", parseInt(PatientId), (message), parseInt(DoctorId), (User_role)).catch(err => console.error("Error calling LoadChatFunctionDoctor:", err));
 }
 document.addEventListener("DOMContentLoaded", () => {
     const loadchatlink = document.querySelectorAll(".load-chat-link");
@@ -54,6 +57,53 @@ document.addEventListener("DOMContentLoaded", () => {
             window.location.href = "/Login_Signup/Login";
         }
         });
+    });
+    /////////////////////////////////////////////////////////////////
+    document.getElementById("btn_send").addEventListener("click", () => {
+        const message = document.getElementById("messageInput").value;
+        const newParagraph = document.createElement("p");
+        newParagraph.className = "sent";
+        newParagraph.textContent = message;
+        document.querySelector(".message-container").appendChild(newParagraph);
+        sendMessage(parseInt(PatientId), message, parseInt(DoctorId), User_role);
+        document.getElementById("messageInput").value = "";
+    });
+    if (connection.state === signalR.HubConnectionState.Connected) {
+        stopConnection1().then(() => {
+            startConnection();
+        });
+    } else {
+        startConnection1();
+    }
+    function stopConnection1() {
+        return connection.stop()
+            .then(() => {
+                console.log("Connection stopped");
+                clearMessageContainer();
+            })
+            .catch(err => console.error(err));
+    }
+    function startConnection1() {
+        return connection.start().then(() => {
+            console.log("Connection started");
+        }).catch(err => console.error(err));
+    }
+    connection.on("ReceiveMessageDoctor", (senderId, message) => {
+        if (parseInt(senderId) == parseInt(DoctorId) && message.userRole !== User_role) {
+            displayMessage(message.messageContent);
+        }
+        else {
+            console.log("User not found");
+        }
+        function displayMessage(message) {
+            if (message === null) {
+                console.log("No message to display from ")
+            }
+            const newParagraph = document.createElement("p");
+            newParagraph.className = "received-gradient";
+            newParagraph.textContent = message;
+            document.querySelector(".message-container").appendChild(newParagraph);
+        }
     });
     function getUserCookie(name) {
         const value = `; ${document.cookie}`;
